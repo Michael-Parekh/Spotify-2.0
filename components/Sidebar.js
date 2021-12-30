9ddaf75cffdf8bdbd33c9c2198c61dac89018503
Michael-Parekh <mparekh4@illinois.edu>
2021-12-29 22:59:24 -0600

    create the sidebar component

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -3,15 +3,57 @@ import {
   SearchIcon,
   LibraryIcon,
   PlusCircleIcon,
+  HeartIcon,
+  RssIcon,
 } from '@heroicons/react/outline';
 
 function Sidebar() {
   return (
-    <div>
-      <div>
-        <button>
+    <div className='text-gray-500 p-5 text-sm border-r border-gray-900'>
+      <div className='space-y-4'>
+        <button className='flex items-center space-x-2 hover:text-white'>
           <HomeIcon className='h-5 w-5' />
+          <p>Home</p>
         </button>
+        <button className='flex items-center space-x-2 hover:text-white'>
+          <SearchIcon className='h-5 w-5' />
+          <p>Search</p>
+        </button>
+        <button className='flex items-center space-x-2 hover:text-white'>
+          <LibraryIcon className='h-5 w-5' />
+          <p>Your Library</p>
+        </button>
+        {/* Just-in-Time Mode allows for custom values and Tailwind will create the utility class on the fly (https://v2.tailwindcss.com/docs/just-in-time-mode). */}
+        <hr className='border-t-[0.1px] border-gray-900' />
+
+        <button className='flex items-center space-x-2 hover:text-white'>
+          <PlusCircleIcon className='h-5 w-5' />
+          <p>Create Playlist</p>
+        </button>
+        <button className='flex items-center space-x-2 hover:text-white'>
+          <HeartIcon className='h-5 w-5' />
+          <p>Liked Songs</p>
+        </button>
+        <button className='flex items-center space-x-2 hover:text-white'>
+          <RssIcon className='h-5 w-5' />
+          <p>Your Episodes</p>
+        </button>
+        <hr className='border-t-[0.1px] border-gray-900' />
+
+        {/* Playlists */}
+        <p className='cursor-pointer hover:text-white'>Playlist name...</p>
+        <p className='cursor-pointer hover:text-white'>Playlist name...</p>
+        <p className='cursor-pointer hover:text-white'>Playlist name...</p>
+        <p className='cursor-pointer hover:text-white'>Playlist name...</p>
+        <p className='cursor-pointer hover:text-white'>Playlist name...</p>
+        <p className='cursor-pointer hover:text-white'>Playlist name...</p>
+        <p className='cursor-pointer hover:text-white'>Playlist name...</p>
+        <p className='cursor-pointer hover:text-white'>Playlist name...</p>
+        <p className='cursor-pointer hover:text-white'>Playlist name...</p>
+        <p className='cursor-pointer hover:text-white'>Playlist name...</p>
+        <p className='cursor-pointer hover:text-white'>Playlist name...</p>
+        <p className='cursor-pointer hover:text-white'>Playlist name...</p>
+        <p className='cursor-pointer hover:text-white'>Playlist name...</p>
       </div>
     </div>
   );
--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,8 @@ import Sidebar from '../components/Sidebar';
 // With Next.js, we have a built in router so React Router is not needed. Next.js uses the filenames to determine the page that we are visiting (e.g. "search.js" is "/search").
 export default function Home() {
   return (
-    <div className=''>
+    <div className='bg-black h-screen overflow-hidden'>
+      {/* Set the above container to the height of the screen and make its overflow hidden so that we can scroll without changing anything else. */}
       <main>
         <Sidebar />
         {/* Center */}
